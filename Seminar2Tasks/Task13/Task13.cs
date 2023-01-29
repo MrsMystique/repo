5f264f4da90c23da4609d291447da02d160df33b
Mystique <zorinatrader@gmail.com>
2023-01-29 21:08:49 +0300

    2 задачи готовы. второй семинар домашка

---
+++ Seminar2Tasks/Task13/Task13.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет.*/
+
+Console.Clear ();
+Console.Write ("Введите трехзначное число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2;
+while (num1/100<1 || num1/100>9 )
+{
+    Console.Write("Вы ошиблись!Введите трехзначное число: ");
+    num1 = Convert.ToInt32(Console.ReadLine());
+}
+if (num1/100>=1)
+{
+    num2 = num1 % 100; 
+    Console.WriteLine ($"В этом числе в разряде единиц цифра {num2%10}");  
+}
+
