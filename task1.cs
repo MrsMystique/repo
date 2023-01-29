e4a65bc93f17db674854aa2e2062aab43991d6d1
Mystique <zorinatrader@gmail.com>
2023-01-29 11:06:53 +0300

    создан репо и гит игнор

---
+++ task1.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число1: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число2: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+if(n==m*m)
+{
+Console.WriteLine("Первое число является квадратом второго");
+}
+else if(m==n*n)
+{
+Console.WriteLine("Второе число является квадратом первого");
+}
+else
+{
+Console.WriteLine("Ни одно число не является квадратом второго");
+}
+
+
