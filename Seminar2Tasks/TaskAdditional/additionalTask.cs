0373fc4c399a233cdbf0484c3c6542dd7558a00c
Mystique <zorinatrader@gmail.com>
2023-01-30 17:05:48 +0300

    готова домашка +доп 30.01

---
+++ Seminar2Tasks/TaskAdditional/additionalTask.cs
@@ -0,0 +1,28 @@
+﻿// https://acmp.ru/asp/do/index.asp?main=task&id_course=1&id_section=3&id_topic=35&id_problem=223
+
+Console.Clear();
+int n = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+int max2 = 0;
+
+
+while(n != 0)
+{  
+   if (max < n)
+   { 
+      max2 = max;
+      max = n;
+   }
+  
+   n = Convert.ToInt32(Console.ReadLine()); 
+
+   if (n > max2 && n < max)
+   {
+      max2 = n;    
+   }
+}
+
+Console.Write(max2);
+
+
+       
